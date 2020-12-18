83af15b644bc8108d122bbbb91bf6bcee65c05b7
ccsxw6 <ccsxw6@gmail.com>
2020-12-17 21:24:22 -0600

    added a buncha shit

--- script.js
+++ script.js
@@ -18,16 +18,25 @@ $(document).ready(function() {
             url: queryUrl,
             method: "GET"
           }).then(function(response) {
+              console.log("recipe response")
               console.log(response)
 
+              //name
               $("#recipeName").html("Food: " + response.hits[0].recipe.label)
 
-              var imgUrl = response.hits[0].recipe.image
-              var image = $("#image").attr("src", imgUrl);
-              $("#image").append(image) //image isn't appearing :'(
-            
-              $("#ingredients").html("Ingredients: " + response.hits[0].recipe.ingredientLines[0])//how to get all ingredients to display? 
 
+              //recipe image
+              var imgUrl = response.hits[0].recipe.image
+              console.log("recipe image")
+              console.log(imgUrl)
+
+              var newRecipeImage = $("<img>")
+              newRecipeImage.attr("src", imgUrl);
+              $("#image").append(newRecipeImage) //image isn't appearing 
+
+              for (var i = 0; i < response.hits[0].recipe.ingredientLines.length; i++) {
+               $("#ingredients").append("Ingredients: " + response.hits[0].recipe.ingredientLines[i] + "</br>") 
+              }
           })
         }
 
@@ -40,11 +49,42 @@ $(document).ready(function() {
               url: queryUrl,
               method: "GET"
             }).then(function(response) {
+              console.log("first music response")
               console.log(response)
 
-              $("#songName").html("Title: " + response.results.trackmatches.track[0].name)
-              $("#artistName").html("Artist: " + response.results.trackmatches.track[0].artist)
-              $("#url").html(response.results.trackmatches.track[0].url) //unable to get image :(
+              for (var i=0; i < response.results.trackmatches.track.length; i++) {
+                if (response.results.trackmatches.track[i].mbid != '') {
+                var mbId = response.results.trackmatches.track[i].mbid
+                $("#songName").html("Title: " + response.results.trackmatches.track[i].name)
+                $("#artistName").html("Artist: " + response.results.trackmatches.track[i].artist)
+                $("#url").html(response.results.trackmatches.track[i].url) 
+                break //break out da for loop
+              }
+            }
+
+              console.log(mbId)
+
+              //instead of userinput use mbid
+
+              var newQueryUrl = "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=5768b380e9979f19a9f79e71ea2d714e&mbid=" + mbId + "&format=json"
+                
+              console.log(newQueryUrl)
+              $.ajax({
+                  url: newQueryUrl, //another url that we build with mbid
+                  method: "GET"
+                }).then(function(response) {
+                  console.log("second music response")
+                  console.log(response)
+    
+                  //take image from second music response and then append it 
+                   var musicImage = response.track.album.image[3]
+                   console.log("mbid image")
+                   console.log(musicImage["#text"])
+                   var newImgtag = $("<img>")
+                   newImgtag.attr("src", musicImage["#text"])
+                   $("#imageContainer").append(newImgtag)
+
+                })
             })
           }
 })
