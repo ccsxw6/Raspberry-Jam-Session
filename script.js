78a8eb425ecac3562e96d79ad2f011ebe6fec5e9
ccsxw6 <ccsxw6@gmail.com>
2020-12-22 21:14:43 -0600

    added a modal

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-$(document).ready(function() {
-  
+$(document).ready(function () {
+
+
+  $('.modal').modal()
+
   //trying to get x button to clear what's in input field
   //tried using .val, .html, setting variable to ""
   // not working :(
-  $("#close-icon").on("click", function() {
+  $("#close-icon").on("click", function () {
     var inputVal = $("#search").val()
     inputVal.empty()
   })
@@ -11,157 +14,175 @@ $(document).ready(function() {
 
   //trying to get enter button to do search
   //not working :(
-  $("#search").keyup(function(e){
-    if(e.keyCode == 13){
-    $("#search-icon").click();
-    // recipeApi()
-    // musicApi()
+  $("#search").keyup(function (e) {
+    if (e.keyCode == 13) {
+      $("#search-icon").click();
+      // recipeApi()
+      // musicApi()
     }
-});
+  });
 
 
-  $("#search-icon").on("click", function() {
-      userInput = $("#search").val() //global variable
-      recipeApi()
-      musicApi()
-    })
+  $("#search-icon").on("click", function () {
+    userInput = $("#search").val() //global variable
+    recipeApi()
+   
+  })
 
   //function for edamam API
-  var recipeApi = function() { 
-      var appId = "a41bfcaf"
-      var appKey = "a4a5f3bd344761256404ebe6a31f8483";
-      var queryUrl = " https://api.edamam.com/search?q=" + userInput + "&app_id=" + appId + "&app_key=" + appKey;
-      
-        $.ajax({
-            url: queryUrl,
-            method: "GET"
-          }).then(function(response) {
-              console.log("recipe response")
-              console.log(response)
-            
+  var recipeApi = function () {
+    var appId = "a41bfcaf"
+    var appKey = "a4a5f3bd344761256404ebe6a31f8483";
+    var queryUrl = " https://api.edamam.com/search?q=" + userInput + "&app_id=" + appId + "&app_key=" + appKey;
 
-            // string interpolation thing
-              var newRecipe = `<div class="row">
-              <div class="col s12 m7">
-                <div class="card">
-                  <div class="card-image">
-                    <img src="${response.hits[0].recipe.image}" id="recipeImage">
+    $.ajax({
+      url: queryUrl,
+      method: "GET"
+    }).then(function (response) {
+      console.log("recipe response")
+      console.log(response)
+
+
+      if (response.count === 0) {
+        $('.modal').modal("open");
+        return;
+      }
+
+      musicApi()
+
+
+      // string interpolation thing
+      //get rid of row here? 
+      // make one container to prepend cards to
+      var newRecipe =
+        `<div class="card">
+                  <div class="row">
+                   <div class="col s6" id="imageRecipe">
+                    <div class="card-image">
+                        <img src="${response.hits[0].recipe.image}" id="recipeImage">
+                      </div>
+                     </div> 
+                    
+                  <div class="col s6" id="recipeInfo">
+                      <div class="card-content">
+                        <!-- Recipe Name -->
+                        <h4>Food: </h4>
+                        <span class="card-title">${response.hits[0].recipe.label}</span>
+
+                        <h5>Ingredients: </h5>
+                        <ul id="ingredients"></ul>
                     </div>
-                  <div class="card-content">
-                    <!-- Recipe Name -->
-                    <h4>Food: </h4>
-                    <span class="card-title">${response.hits[0].recipe.label}</span>
-
-                    <h5>Ingredients: </h5>
-                    <ul id="ingredients"></ul>
-                </div>
-                  <div class="card-action">
-                    <a href="${response.hits[0].recipe.url}">Recipe Wesbite</a>
+                      <div class="card-action">
+                        <a href="${response.hits[0].recipe.url}">Recipe Wesbite</a>
+                   </div>
                   </div>
-                </div>
-              </div>
-            </div>`
+                </div>`
 
-            $("#recipeContainer").prepend(newRecipe)
-            
+      $("#recipeContainer").prepend(newRecipe)
 
-              for (var i = 0; i < response.hits[0].recipe.ingredientLines.length; i++) {
-                $("#ingredients").append(response.hits[0].recipe.ingredientLines[i] + "</br>") 
-              }
-          })
-        }
+
+      for (var i = 0; i < response.hits[0].recipe.ingredientLines.length; i++) {
+        $("#ingredients").append(response.hits[0].recipe.ingredientLines[i] + "</br>")
+      }
+    })
+  }
 
   //function for last.fm
-    var musicApi = function() {
-        var apiKey = "5768b380e9979f19a9f79e71ea2d714e";
-        var queryUrl = "http://ws.audioscrobbler.com/2.0/?method=track.search&track=" + userInput + "&api_key=" + apiKey + "&format=json"
-        
-            $.ajax({
-              url: queryUrl,
-              method: "GET"
-            }).then(function(response) {
-              console.log("first music response")
-              console.log(response)
+  var musicApi = function () {
+    var apiKey = "5768b380e9979f19a9f79e71ea2d714e";
+    var queryUrl = "http://ws.audioscrobbler.com/2.0/?method=track.search&track=" + userInput + "&api_key=" + apiKey + "&format=json"
+
+    $.ajax({
+      url: queryUrl,
+      method: "GET"
+    }).then(function (response) {
+      console.log("first music response")
+      console.log(response)
 
 
-              var newMusic = `<div class="row">
-              <div class="col s12 m7">
-                <div class="card">
+      var newMusic =
+        `<div class="card">
+                <div class="row">
                   <div class="card-image">
-                    <img id="musicImage"> 
+                   <div class="col s6" id="albumImage">
+                     <img id="musicImage"> 
                     </div>
-                  <div class="card-content">
+                   </div> 
+                  <div class="col s6" id="musicInfo">
+                   <div class="card-content">
                     <h4>Title: </h4>
+
                     <span class="card-title" id="title"></span>
+
                     <h5>Artist: </h5>
+
                     <span class="card-title" id="artist"></span>
-                </div>
-                  <div class="card-action">
-                    <a id="music-url">Music Wesbite</a>
+                
+                    <div class="card-action">
+                      <a id="music-url">Music Wesbite</a>
+                    </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-            </div>`
+                  </div>
+                </div>`
 
-            $("#artistContainer").prepend(newMusic)
-
+      $("#artistContainer").prepend(newMusic)
 
 
-              //for loop to see if result has bid
-              for (var i=0; i < response.results.trackmatches.track.length; i++) {
-                if (response.results.trackmatches.track[i].mbid != '') {
 
-                var mbId = response.results.trackmatches.track[i].mbid
+      //for loop to see if result has bid
+      for (var i = 0; i < response.results.trackmatches.track.length; i++) {
+        if (response.results.trackmatches.track[i].mbid != '') {
 
-                //music title
-                $("#title").append(response.results.trackmatches.track[i].name)
-                
+          var mbId = response.results.trackmatches.track[i].mbid
 
-                //music artist
-                $("#artist").append(response.results.trackmatches.track[i].artist)
-                
-                //music url
-                var responseUrl = response.results.trackmatches.track[i].url
-                var urlAtt = $("#music-url").attr("href", responseUrl)
-                $("#music-url").append(urlAtt)
+          //music title
+          $("#title").append(response.results.trackmatches.track[i].name)
 
-                break //break out da for loop
-              }
-            }
 
-              console.log(mbId)
+          //music artist
+          $("#artist").append(response.results.trackmatches.track[i].artist)
 
-              //using mbid to get image
-              var newQueryUrl = "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=5768b380e9979f19a9f79e71ea2d714e&mbid=" + mbId + "&format=json"
-                
-              console.log(newQueryUrl)
-              $.ajax({
-                  url: newQueryUrl,
-                  method: "GET"
-                }).then(function(response) {
-                  console.log("second music response")
-                  console.log(response)
-    
-                  //take image from second music response and then append it 
-                   var musicImage = response.track.album.image[3]
-                   console.log("mbid image")
-                   console.log(musicImage["#text"])
+          //music url
+          var responseUrl = response.results.trackmatches.track[i].url
+          var urlAtt = $("#music-url").attr("href", responseUrl)
+          $("#music-url").append(urlAtt)
 
-                   $("#musicImage").attr("src", musicImage["#text"])
-                   $("#musicImage").append(musicImage)
+          break //break out da for loop
+        }
+      }
 
-                })
-            })
-          }
+      console.log(mbId)
+
+      //using mbid to get image
+      var newQueryUrl = "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=5768b380e9979f19a9f79e71ea2d714e&mbid=" + mbId + "&format=json"
+
+      console.log(newQueryUrl)
+      $.ajax({
+        url: newQueryUrl,
+        method: "GET"
+      }).then(function (response) {
+        console.log("second music response")
+        console.log(response)
+
+        //take image from second music response and then append it 
+        var musicImage = response.track.album.image[3]
+        console.log("mbid image")
+        console.log(musicImage["#text"])
+
+        $("#musicImage").attr("src", musicImage["#text"])
+        $("#musicImage").append(musicImage)
+
+      })
+    })
+  }
 
 })
 
 
 
 //TO DO: 
-  // modal for if userinput doesn't work - couldn't get modal to work - tried a bunch of stuff
-  // fix search button and make it work when hitting enters
   // x button needs to clear what's in input
-  // get cards to line up?? The first to do, but the rest don't line up
   // if they enter the same food again, get a different food and recipe with same name?? Might be too ambitious...
-  // NOT VERY MOBILE FRIENDLY!!!!!! Need the cards to move underneath each other? Everything else looks okay
+  //rearrange folders - media folder and an asset folder - all html should be in same place
+  
